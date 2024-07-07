7e21f6bfbd407831d5263e7c11e98912e8e9f77a
BloodSymph <rubakharoma@gmail.com>
2024-07-07 11:55:28 +0300

    order service v3

---
+++ order-service/src/main/java/com/company/order/service/client/OrderClientService.java
@@ -0,0 +1,14 @@
+package com.company.order.service.client;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface OrderClientService {
+
+    //Create new Order
+
+    //Show order
+
+    //Remove order
+
+}
---
+++ order-service/src/main/java/com/company/order/service/client/implementation/OrderClientServiceImpl.java
@@ -0,0 +1,13 @@
+package com.company.order.service.client.implementation;
+
+import com.company.order.service.client.OrderClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderClientServiceImpl implements OrderClientService {
+
+
+
+}
