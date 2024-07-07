2934917a1d6b8240741fc1dfb4ae1b6d50307a20
BloodSymph <rubakharoma@gmail.com>
2024-07-07 13:57:23 +0300

    order service v4

--- order-service/src/main/java/com/company/order/repository/OrderRepository.java
+++ order-service/src/main/java/com/company/order/repository/OrderRepository.java
@@ -1,11 +1,22 @@
 package com.company.order.repository;
 
 import com.company.order.entity.OrderEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+    @EntityGraph(value = "order-entity-graph-with-ordered-items", type = EntityGraph.EntityGraphType.FETCH)
+    @Query("SELECT orders FROM Order orders WHERE orders.profileId = :profileId")
+    Optional<OrderEntity> showOrderByProfileId(
+            @Param(value = "profileId") Long profileId
+    );
 
     void deleteByProfileId(Long profileId);
 
