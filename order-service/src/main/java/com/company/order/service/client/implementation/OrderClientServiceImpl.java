91e5d5180c225abd7939c149f25de6e1bb2d29ad
BloodSymph <rubakharoma@gmail.com>
2024-07-07 12:05:42 +0300

    order service v2

--- order-service/src/main/java/com/company/order/service/client/implementation/OrderClientServiceImpl.java
+++ order-service/src/main/java/com/company/order/service/client/implementation/OrderClientServiceImpl.java
@@ -1,5 +1,8 @@
 package com.company.order.service.client.implementation;
 
+import com.company.order.feign.CartFeignClient;
+import com.company.order.repository.OrderRepository;
+import com.company.order.repository.OrderedItemsRepository;
 import com.company.order.service.client.OrderClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,6 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderClientServiceImpl implements OrderClientService {
+
+    private final OrderRepository orderRepository;
+
+    private final OrderedItemsRepository orderedItemsRepository;
+
+    private final CartFeignClient cartFeignClient;
 
 
 
