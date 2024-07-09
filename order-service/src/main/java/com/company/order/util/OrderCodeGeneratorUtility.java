3a598d102c78d4c82996611d0d5832a409bb5c08
BloodSymph <rubakharoma@gmail.com>
2024-07-09 14:18:16 +0300

    order service v5

--- order-service/src/main/java/com/company/order/util/OrderCodeGeneratorUtility.java
+++ order-service/src/main/java/com/company/order/util/OrderCodeGeneratorUtility.java
@@ -2,9 +2,13 @@ package com.company.order.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.stream.IntStream;
+
 @UtilityClass
 public class OrderCodeGeneratorUtility {
 
-
+    public static Integer orderCodeGenerate() {
+        return null;
+    }
 
 }
