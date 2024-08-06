f9929307039ab07dcc865aacca77947e30848922
BloodSymph <rubakharoma@gmail.com>
2024-08-06 19:52:02 +0300

    review service v3

--- review-service/src/main/java/com/company/review/service/client/ReviewClientService.java
+++ review-service/src/main/java/com/company/review/service/client/ReviewClientService.java
@@ -1,10 +1,26 @@
 package com.company.review.service.client;
 
+import com.company.review.dto.client.ReviewClientRequest;
+import com.company.review.dto.client.ReviewClientResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ReviewClientService {
 
+    Page<ReviewClientResponse> getAllReviews(Pageable pageable);
 
+    ReviewClientResponse addReview(
+            ReviewClientRequest reviewClientRequest, String productUrl
+    );
+
+    void deleteReview(
+            Long profileId, Long reviewVersion
+    );
+
+    @Scheduled(fixedRate = 120)
+    void evictAllCacheWithTime();
 
 }
--- review-service/src/main/java/com/company/review/service/client/implementation/ReviewClientServiceImpl.java
+++ review-service/src/main/java/com/company/review/service/client/implementation/ReviewClientServiceImpl.java
@@ -1,17 +1,60 @@
 package com.company.review.service.client.implementation;
 
+import com.company.review.dto.client.ReviewClientRequest;
+import com.company.review.dto.client.ReviewClientResponse;
+import com.company.review.entity.ReviewEntity;
+import com.company.review.feign.ProductFeignClient;
+import com.company.review.mapper.client.ReviewClientMapper;
 import com.company.review.repository.ReviewRepository;
 import com.company.review.service.client.ReviewClientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.company.review.mapper.client.ReviewClientMapper.mapReviewClientRequestToEntity;
+import static com.company.review.mapper.client.ReviewClientMapper.mapToReviewClientResponse;
+import static com.company.review.util.CacheEvictUtility.evictAllCaches;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = {"reviews_client"})
 public class ReviewClientServiceImpl implements ReviewClientService {
 
     private final ReviewRepository reviewRepository;
 
-    private final RatingRepository ratingRepository;
+    private final ProductFeignClient productFeignClient;
 
+    @Override
+    @Cacheable(unless = "#result == null ")
+    public Page<ReviewClientResponse> getAllReviews(Pageable pageable) {
+        return reviewRepository
+                .findAll(pageable)
+                .map(ReviewClientMapper::mapToReviewClientResponse);
+    }
 
+    @Override
+    @Transactional
+    public ReviewClientResponse addReview(
+            ReviewClientRequest reviewClientRequest, String productUrl) {
+        ReviewEntity review = mapReviewClientRequestToEntity(reviewClientRequest);
+        review.setProductId(productFeignClient.getProductIdForReview(productUrl));
+        return mapToReviewClientResponse(review);
+    }
+
+    @Override
+    @Transactional
+    public void deleteReview(Long profileId, Long reviewVersion) {
+
+    }
+
+    @Override
+    public void evictAllCacheWithTime() {
+        evictAllCaches();
+    }
 }
