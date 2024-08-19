a194d3e893e9c234cc17b85f9de5c4f9e010beca
BloodSymph <rubakharoma@gmail.com>
2024-08-19 20:58:00 +0300

    starting make api gateway v2

---
+++ api-gateway/src/main/java/com/company/gateway/ApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.company.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCaching
+@EnableScheduling
+@EnableFeignClients
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
